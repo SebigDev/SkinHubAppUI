9142dc78c18034b2e48d863d04e6523155833904
Joseph <jonathanadahjoe@gmail.com>
2018-09-17 09:20:49 +0100

    registration page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './layout/app.component';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HomeComponent, 
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'sign-in', component: RegistrationComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-registration', 
+    templateUrl: './registration.component.html'
+})
+
+export class RegistrationComponent {
+    sign_in: boolean = true;
+}
