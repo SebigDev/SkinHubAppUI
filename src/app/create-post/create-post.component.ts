1966be4fde83d6f37505cfc44cba223fdb01b6b7
Joseph <jonathanadahjoe@gmail.com>
2018-09-25 21:25:05 +0100

    add get genders method in service

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../posts/post.service';
+import { Gender } from '../common/models/gender';
 
 @Component({
   selector: 'app-create-post',
@@ -8,11 +10,16 @@ import { Component, OnInit } from '@angular/core';
 export class CreatePostComponent implements OnInit {
 
   page: number = 1;
-  progressBarColor: string = 'white';
+  allGenders = [];
+  selectedGenderId: number;
 
-  constructor() { }
+  constructor(private _postService: PostService) { }
 
   ngOnInit() {
+    this._postService.getAllGenders()
+        .subscribe(data => {
+          this.allGenders = data;
+        });
   }
 
   previousPage() {
@@ -27,29 +34,11 @@ export class CreatePostComponent implements OnInit {
     return ((this.page - 1) * 25) + '%';
   }
 
-  getProgressColor() {
-    switch (this.page) {
-      case 1: 
-        this.progressBarColor = 'white';       
-        break;
-      case 2: 
-        this.progressBarColor = 'red';
-        break;
-      case 3: 
-        this.progressBarColor = 'yellow';
-        break;
-      case 4: 
-        this.progressBarColor = 'orange';
-        break;
-      case 5: 
-        this.progressBarColor = 'green';
-        break;
-      default:
-        this.progressBarColor = 'white';
-        break;
-    }
-
-    return this.progressBarColor;
+  genderSelected(g: Gender) {
+    this.selectedGenderId = g.id;
+    console.log(this.selectedGenderId);
+    
+    this.page++;
   }
 
 }
